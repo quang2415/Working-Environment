77b1f9f90cfe01b3f39e197fbf111a0cffd204d4
quang2415 <34291933+quang2415@users.noreply.github.com>
2019-10-29 14:18:38 +0700

    Update distance2

--- ProtrasPython/distance2.py
+++ ProtrasPython/distance2.py
@@ -6,5 +6,11 @@ def distance2(X, Y, *A):
         A = np.identity(X.shape[1])
 
     # D = math.sqrt((-2*X*A*Y.getH())+(X*A*X).sum(axis=1) + (Y*A*Y).sum(axis=1))
+
+
+    Y = np.asarray(Y)
+
+    Y = Y.reshape(7,1)
+
     D = (-2*X*A) * Y.getH()
     print(D)
